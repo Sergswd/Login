b22d331039dd2467783e2a6fd74f53e88f3079fb
Sergey Kh <falko.serega2016@yandex.ru>
2021-06-21 14:23:47 +0300

    output notification

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import '../css/style.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import '../css/style.css'
 
-import UI from './config/ui.config';
-import { validate } from './helpers/validate';
-import { showInputError, removeInputError } from './views/form';
+import UI from './config/ui.config'
+import { validate } from './helpers/validate'
+import { showInputError, removeInputError } from './views/form'
+import { login } from './services/auth.service'
+import { notify } from './views/notifications'
 
 
 const { form, inputEmail, inputPassword } = UI;
@@ -17,7 +19,7 @@ form.addEventListener('submit', e => {
 inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
 
 // Handlers
-function onSubmit() {
+async function onSubmit() {
   const isValidForm = inputs.every(el => {
     const isValidInput = validate(el);
     if (!isValidInput) {
@@ -26,6 +28,16 @@ function onSubmit() {
 
     return isValidInput;
   });
+
+  if (!isValidForm) return; 
+
+  try {
+    await login(inputEmail.value, inputPassword.value)
+    form.reset()
+    notify({ msg: 'Login success', className: 'alert-success' })
+  } catch (err) {
+    notify({ msg: 'Login faild', className: 'alert-danger' })
+  }
 }
 
 
